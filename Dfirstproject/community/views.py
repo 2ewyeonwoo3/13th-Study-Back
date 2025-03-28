5d8a581ac00bd1d832cc31efff2b85a9dd7c390b
Hyerin Sung <hyerinx@naver.com>
2025-03-29 00:53:50 +0900

    ♻️ views.py, urls.py import문 수정
    community.models 전체 로딩

--- Dfirstproject/community/views.py
+++ Dfirstproject/community/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from community.models import Post
+from community.models import *
 
 # Create your views here.
 
--- Dfirstproject/firstproject/urls.py
+++ Dfirstproject/firstproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from community.views import List, detail
+from community.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
