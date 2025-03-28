a58fd297854535cd90c6d05ba127a4182a11f8bd
Hyerin Sung <hyerinx@naver.com>
2025-03-29 00:58:50 +0900

    ♻️ admin 페이지 모델 등록 로직 개선

--- Dfirstproject/community/admin.py
+++ Dfirstproject/community/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import *
+import community.models as Models
 
 # Register your models here.
 
-admin.site.register(Post)
+for Model in Models:
+    admin.site.register(Model)
