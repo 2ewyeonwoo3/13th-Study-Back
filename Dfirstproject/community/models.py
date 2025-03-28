ff75b5a0cc8edb8b02092f3a8beb71486afc99e4
Hyerin Sung <hyerinx@naver.com>
2025-03-29 00:57:06 +0900

    🔨 Post 모델 upload_time 필드 default 추가

--- Dfirstproject/community/models.py
+++ Dfirstproject/community/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField('Title', max_length=50, blank=True)
-    upload_time = models.DateTimeField(unique=True)
+    upload_time = models.DateTimeField(unique=True, default=timezone.now())
     content = models.TextField('Content')
 
     def __str__(self):
