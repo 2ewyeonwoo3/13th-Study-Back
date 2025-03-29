f6c83e78b07d9f46ebceb36a5ac7183ee65dee3e
Hyerin Sung <hyerinx@naver.com>
2025-03-29 12:33:25 +0900

    🚚 template 파일 경로 변경
    개별 페이지 파일들을 폴더로 정리함.

--- Dfirstproject/community/views.py
+++ Dfirstproject/community/views.py
@@ -6,16 +6,16 @@ from community.models import *
 
 def list(request):
     posts = Post.objects.filter(upload_time__lte = timezone.now()).order_by('upload_time')
-    return render(request, 'list.html', {'posts':posts})
+    return render(request, 'pages/list.html', {'posts':posts})
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'detail.html', {'post':post})
+    return render(request, 'pages/detail.html', {'post':post})
 
 def question_list(request):
     questions = Question.objects.only('title', 'upload_time', 'name', 'status').order_by('upload_time')
-    return render(request, 'question_list.html', {'questions':questions})
+    return render(request, 'pages/question_list.html', {'questions':questions})
 
 def question_detail(request, pk):
     question = get_object_or_404(Question, pk=pk)
-    return render(request, 'question_detail.html', {'question':question})
+    return render(request, 'pages/question_detail.html', {'question':question})
