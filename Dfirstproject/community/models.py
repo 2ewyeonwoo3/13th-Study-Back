f37526e36c13dc6ab96e84315b5e4cf29001ca48
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-03-29 11:43:04 +0900

    question model 오류

--- Dfirstproject/community/models.py
+++ Dfirstproject/community/models.py
@@ -5,7 +5,16 @@ from django.db import models
 class Post(models.Model):
   title = models.CharField('Title', max_length =50, blank = True)
   upload_time = models.DateTimeField(unique=True)
-  content = models.TextField('content')
+  content = models.TextField('Content')
 
+  def __str__(self):
+    return self.title
+
+
+class Question(models.Model):
+  title = models.CharField('Title', max_length =50, blank = True)
+  upload_time = models.DateTimeField(unique=True)
+  content = models.TextField('Content')
+  
   def __str__(self):
     return self.title
