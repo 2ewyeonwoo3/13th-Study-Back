a31401759db44a6d9f78753be0771de28557e659
Hyerin Sung <hyerinx@naver.com>
2025-04-01 02:09:53 +0900

    🔨 View 수정
    - Community 모델 반영
    - question_list 뷰 쿼리스트링 필터링 로직 추가
    - question_detail 뷰 help_questions 리턴값 추가

--- Dfirstproject/community/views.py
+++ Dfirstproject/community/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from community.models import *
+from community.models import Community,Question
 
 # Create your views here.
 
 def list(request):
-    posts = Post.objects.filter(upload_time__lte = timezone.now()).order_by('upload_time')
+    posts = Community.objects.filter(upload_time__lte = timezone.now()).order_by('upload_time')
     return render(request, 'pages/list.html', {'posts':posts})
 
 def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Community, pk=pk)
     return render(request, 'pages/detail.html', {'post':post})
 
 def question_list(request):
-    questions = Question.objects.only('title', 'upload_time', 'name', 'status').order_by('upload_time')
+    query_string_상태 = request.GET.get('상태')
+    if query_string_상태 == '도와주세요':
+        상태 = False
+    elif query_string_상태 == '해결됐어요':
+        상태 = True
+    else:
+        상태 = None
+    if 상태 != None:
+        questions = Question.objects.filter(status = 상태).only('title', 'upload_time', 'name', 'status').order_by('-upload_time')
+    else:
+        questions = Question.objects.only('title', 'upload_time', 'name', 'status').order_by('-upload_time')
     return render(request, 'pages/question_list.html', {'questions':questions})
 
 def question_detail(request, pk):
     question = get_object_or_404(Question, pk=pk)
-    return render(request, 'pages/question_detail.html', {'question':question})
+    help_questions = Question.objects.filter(status = False).only('status').order_by('-upload_time')
+    return render(request, 'pages/question_detail.html', {'question':question, 'help_questions':help_questions})
